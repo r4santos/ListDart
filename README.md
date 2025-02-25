37a3515928287ba6ef115b0b086d5bdb4fc3e1d2
Rafael Santos <rafaelsantoslp11@gmail.com>
2025-02-25 18:15:39 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ListDart
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/list1_test.dart
@@ -0,0 +1,8 @@
+import 'package:list1/list1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
